c1c42de402ddb41487ba4f4841c5ace48efa94ed
AlejandroRS123 <alericsan@alu.edu.gva.es>
2026-01-22 20:20:02 +0100

    ADD API hasta el punto 9 terminado

--- app/Http/Controllers/Api/JugadorController.php
+++ app/Http/Controllers/Api/JugadorController.php
@@ -3,18 +3,50 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\JugadorRequest;
+use App\Http\Resources\JugadorResource;
 use App\Models\Jugador;
-use Illuminate\Http\Request;
 
 class JugadorController extends Controller
 {
     public function index()
     {
-        return Jugador::all(); // o Jugador::get()
+        return JugadorResource::collection(
+            Jugador::query()->paginate(10)
+        );
     }
 
     public function show(Jugador $jugador)
     {
-        return $jugador;
+        return new JugadorResource($jugador);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'nom' => ['required', 'string', 'max:255'],
+            'equip_id' => ['required', 'exists:equips,id'],
+            'dorsal' => ['nullable', 'integer', 'min:1', 'max:99'], // dorsal suele ser 1-99
+            'data_naixement' => ['nullable', 'date', 'before_or_equal:today', 'after:1900-01-01'],
+            'foto' => ['nullable', 'url'],
+        ]);
+
+        $jugador = \App\Models\Jugador::create($data);
+
+        return response()->json($jugador->load('equip'), 201);
+    }
+
+    public function update(JugadorRequest $request, Jugador $jugador)
+    {
+        $jugador->update($request->validated());
+
+        return new JugadorResource($jugador);
+    }
+
+    public function destroy(Jugador $jugador)
+    {
+        $jugador->delete();
+
+        return response()->noContent(); // 204
     }
 }
